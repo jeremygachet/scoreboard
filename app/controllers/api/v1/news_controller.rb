313e95ccc805d2461ed6550df644410c3c12fe7d
jeremy <jeremy.gachet@gmail.com>
2018-11-01 12:23:44 +0400

    inverse news ordering

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -2,11 +2,10 @@ module Api::V1
   class NewsController < InheritedResources::Base
 
     def index
-      render json: News.where(published: true).all.order(published_datetime: :desc).limit(200)
+      render json: News.where(published: true).all.order(published_datetime: :asc).limit(200)
     end
 
     private
-
       def news_params
         params.require(:news).permit(:title, :content, :published, :published_datetime)
       end
